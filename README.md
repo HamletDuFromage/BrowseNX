7dc49094f326cb5f5240cfb2c733b63909194a99
flb <f.lb@gmx.com>
2020-09-28 18:55:15 +0200

    Made for fast.com only

--- README.md
+++ README.md
@@ -1,21 +1,6 @@
-# NXBrowser
-**Launches the Internet Browser applet within a NSP.**
-_To build BrowseNX, please supply your own prod.keys._
+# SpeedtextNX
+**Launches fast.com through the Nintendo Switch browser.**
 ## Credit
-- p-sam - Supernag fix via sysmodule included in each release: https://github.com/p-sam/switch-sys-tweak AND wifiwebauthapplet fallback: https://github.com/switchbrew/libnx/commit/8360e561c5e48f7a2a704df3c97657e0d879629b not yet used in a libnx release
-- XorTroll - Working out the browser applet arguments and for the nsp structure/build method.
-- The-4n - Making legal NSP packing easy.
-- Switchbrew - NPDMTool
-## Building
+- crc-32 - the original BrowserNX code
+- XorTroll - Working out the browser applet argument.
 
-### Requirements
- - Libnx
- - switch-tools devkitpro package
- - prod.keys file at the repo root
-
-### Windows
- - You need to have devkitpro installed with msys, including command prompt support for msys-specific commands like `mv`
- - Run `buildwin.bat`
-
-### Linux
- - Run `build.sh`
--- source/main.c
+++ source/main.c
@@ -6,6 +6,8 @@
 // Include the main libnx system header, for Switch development
 #include <switch.h>
 
+#define URL "https://fast.com"
+
 void showError(char* errorText1, char* errorText2, Result outrc)
 {
     AppletHolder err;
@@ -31,154 +33,13 @@ void showError(char* errorText1, char* errorText2, Result outrc)
     exit(1);
 }
 
-SwkbdTextCheckResult validate_text(char* tmp_string, size_t tmp_string_size) {
-    if (strncmp(tmp_string, "https://", 8) !=0 && strncmp(tmp_string, "http://", 7) !=0) {
-        strncpy(tmp_string, "Add 'https://' or 'http://' to url", tmp_string_size);
-        return SwkbdTextCheckResult_Bad;
-    }
-    if(strlen(tmp_string) < 12) {
-        strncpy(tmp_string, "URL too short to be valid", tmp_string_size);
-        return SwkbdTextCheckResult_Bad;
-    }
-    static const char *allowed=":/.%0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char *reset = tmp_string;
-    while(*tmp_string)
-    {
-        if(strchr(allowed, *tmp_string) == NULL){
-            strncpy(reset, "Remove special characters from URL", tmp_string_size);
-            return SwkbdTextCheckResult_Bad;
-        }
-        tmp_string++;
-    }
-
-    return SwkbdTextCheckResult_OK;
-}
-
-int showKeyboard(char out[0xc00], char* title, char* placehold, char* oktxt, char* initial)
-{
-    int rc;
-    SwkbdConfig kbd;
-    char tmpoutstr[0xc00] = {0};
-    rc = swkbdCreate(&kbd, 0);
-    if(R_SUCCEEDED(rc)) {
-        swkbdConfigMakePresetDefault(&kbd);
-        swkbdConfigSetHeaderText(&kbd, title);
-        swkbdConfigSetOkButtonText(&kbd, oktxt);
-        swkbdConfigSetGuideText(&kbd, placehold);
-        swkbdConfigSetInitialText(&kbd, initial);
-        swkbdConfigSetTextCheckCallback(&kbd, validate_text);
-        rc = swkbdShow(&kbd, tmpoutstr, sizeof(tmpoutstr));
-        if(R_SUCCEEDED(rc)) {
-            if(tmpoutstr[0] != '\0') {
-                strcpy(out, tmpoutstr);
-                swkbdClose(&kbd);
-                return 0;
-            }
-        }else if(rc != (Result) 0x5d59) {
-            showError("Software Keyboard Error\nLookup errorcode for more info", "", rc);
-        }
-    }else{
-        showError("Software Keyboard Error\nLookup errorcode for more info", "", rc);
-    }
-    swkbdClose(&kbd);
-    return -1;
-}
-void startAuthApplet(char* url) {
-    Result rc=0;
-    // TODO: Move to libnx impl once its in a stable release
-    WebWifiConfig config;
-    webWifiCreate(&config, NULL, url, (Uuid) {0} , 0);
-    WebWifiReturnValue out;
-    rc = webWifiShow(&config, &out);
-    if(R_FAILED(rc)) {
-        showError("Browser Error\nLookup errorcode for more info", "", rc);
-    }
-}
-
 int main(int argc, char* argv[])
 {
     Result rc=0;
-    int i = 0;
     char url[0xc00] = {0};
-    nsvmInitialize();
-    pminfoInitialize();
-    consoleInit(NULL);
-    strcpy(url, "https://dns.switchbru.com");
-    printf("Press [L] to choose url\n");
-    printf("Press [R] to set default url\n");
-    printf("Press [X] to reset default url\n");
-    printf("Press [-] to launch as auth applet " CONSOLE_RED "(LIMITED FEATURES)");
-    bool nagOn;
-    nsvmNeedsUpdateVulnerability(&nagOn);
-    if(nagOn) {
-        s32 pCount;
-        u64 pids[100];
-        u64 cId;
-        u32 i = 0;
-        bool isPatched = false;
-        svcGetProcessList(&pCount, pids, 100);
-        while (i <= pCount-1) {
-            pminfoGetProgramId(&cId, pids[i]);
-            if(cId == 0x00FF747765616BFF || cId == 0x01FF415446660000) {
-                printf(CONSOLE_GREEN "Supernag enabled, but patched via switch-sys-tweak!\n");
-                isPatched = true;
-                break;
-            }
-            i++;
-        }
-        if(!isPatched){
-            showError("Error: Nag active, check more details", "Browser won't launch if supernag is active\n\nUse gagorder or switch-sys-tweak (the latter is bundled with BrowseNX) to disable supernag.", 0);
-        }
-    }
-    bool forceAuth = false;
-    while(appletMainLoop()) {
-        hidScanInput();
-        u64 kDown = hidKeysHeld(CONTROLLER_P1_AUTO);
-        if(kDown & KEY_L) {
-            showKeyboard(url, "Choose URL", "Enter a URL", "Go", "https://");
-            break;
-        } else if(kDown & KEY_R) {
-            char defUrl[0xc00] = {0};
-            char fURL[0xc00] = {0};
-            FILE* dUCheck = fopen("sdmc:/defUrl.txt", "r");
-            if (dUCheck != NULL) {
-                fgets(fURL, 0xc00, dUCheck);
-            }else {
-                strcpy(fURL, url);
-            }
-            fclose(dUCheck);
-            if(showKeyboard(defUrl, "Set default URL", "Enter A URL", "Set", fURL) == 0) {
-                FILE* dUFile = fopen("sdmc:/defUrl.txt", "w+");
-                fprintf(dUFile, "%s", defUrl);
-                fclose(dUFile);
-            }
-        } else if(kDown & KEY_X) {
-            remove("sdmc:/defUrl.txt");
-        } else if(kDown & KEY_MINUS) {
-            forceAuth = true;
-        }
-        i++;
-        if(i >= 60*2){
-            break;
-        }
-        consoleUpdate(NULL);
-    }
-    consoleExit(NULL);
-    pminfoExit();
-    nsvmExit();
-
-    FILE* dUFile = fopen("sdmc:/defUrl.txt", "r");
-    char fURL[0xc00] = {0};
-    if(dUFile != NULL) {
-        fgets(fURL, 0xc00, dUFile);
-        fclose(dUFile);
-        if(validate_text(fURL, 0xc00) == SwkbdTextCheckResult_OK) {
-            strcpy(url, fURL);
-        }else{
-            showError("Default URL file error, check more details", fURL, 0);
-        }
-    }
-    if(appletGetAppletType() == AppletType_Application && !forceAuth) { // Running as a title
+    strcpy(url, URL);
+    int at = appletGetAppletType();
+    if(at == AppletType_Application) { // Running as a title
         WebCommonConfig conf;
         WebCommonReply out;
         rc = webPageCreate(&conf, url);
